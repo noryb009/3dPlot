6effc48be53829f1d0b717bf8b7c0e401e1d1f44
noryb009 <noryb009@gmail.com>
2014-10-29 10:00:30 -0400

    Make axis coloured

--- main.js
+++ main.js
@@ -7,21 +7,25 @@ var AXIS_TICK_SPACING = 1;
 var axisPositions = function() {
 	"use strict";
 	var posns = [
-		new THREE.Vector3(-AXIS_SIZE, 0, 0),
-		new THREE.Vector3( AXIS_SIZE, 0, 0),
-		new THREE.Vector3(0, -AXIS_SIZE, 0),
-		new THREE.Vector3(0,  AXIS_SIZE, 0),
-		new THREE.Vector3(0, 0, -AXIS_SIZE),
-		new THREE.Vector3(0, 0,  AXIS_SIZE),
+		[new THREE.Vector3(-AXIS_SIZE, 0, 0),
+		 new THREE.Vector3( AXIS_SIZE, 0, 0),],
+		[new THREE.Vector3(0, -AXIS_SIZE, 0),
+		 new THREE.Vector3(0,  AXIS_SIZE, 0),],
+		[new THREE.Vector3(0, 0, -AXIS_SIZE),
+		 new THREE.Vector3(0, 0,  AXIS_SIZE),],
 	];
 
 	for(var i = -AXIS_SIZE; i <= AXIS_SIZE; i += AXIS_TICK_SPACING) {
 		if(i === 0) continue;
-		posns.push(
+		posns[0].push(
 			new THREE.Vector3(i, 0, -AXIS_TICK_SIZE),
-			new THREE.Vector3(i, 0,  AXIS_TICK_SIZE),
+			new THREE.Vector3(i, 0,  AXIS_TICK_SIZE)
+		);
+		posns[1].push(
 			new THREE.Vector3(-AXIS_TICK_SIZE, i, 0),
-			new THREE.Vector3( AXIS_TICK_SIZE, i, 0),
+			new THREE.Vector3( AXIS_TICK_SIZE, i, 0)
+		);
+		posns[2].push(
 			new THREE.Vector3(-AXIS_TICK_SIZE, 0, i),
 			new THREE.Vector3( AXIS_TICK_SIZE, 0, i)
 		);
@@ -33,17 +37,25 @@ window.onload = function() {
 	"use strict";
 	u.init();
 
-	var axisGeo = new THREE.Geometry();
-	axisGeo.vertices = axisPositions();
-	var axisMat = new THREE.LineBasicMaterial({
-		color: 0xffffff,
+	var axisGeo = [new THREE.Geometry(), new THREE.Geometry(), new THREE.Geometry()];
+	var posns = axisPositions();
+	axisGeo[0].vertices = posns[0];
+	axisGeo[1].vertices = posns[1];
+	axisGeo[2].vertices = posns[2];
+	var xMat = new THREE.LineBasicMaterial({
+		color: 0xff0000,
 		transparent: true,
 		opacity: 0.5,
 		depthWrite: false
 	});
-	var axisMesh = new THREE.Line(axisGeo, axisMat, THREE.LinePieces);
+	var yMat = xMat.clone();
+	var zMat = xMat.clone();
+	yMat.setValues({color: 0x00ff00});
+	zMat.setValues({color: 0x0000ff});
 
-	u.addMesh(axisMesh);
+	u.addMesh(new THREE.Line(axisGeo[0], xMat, THREE.LinePieces));
+	u.addMesh(new THREE.Line(axisGeo[1], yMat, THREE.LinePieces));
+	u.addMesh(new THREE.Line(axisGeo[2], zMat, THREE.LinePieces));
 
 	u.render();
 };
